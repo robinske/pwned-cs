76516d4ca8912969380a6bfb2ffa5e068421d461
Kelley Robinson <krobinson@twilio.com>
2018-06-13 18:22:08 -0700

    Example .NET core app using the PwnedPasswords.Client lib

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PwnedPasswords.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+
+            var app = provider.GetService<Application>();
+            await app.Verify("password1");
+
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<Application>();
+            services.AddLogging();
+            services.AddPwnedPasswordHttpClient();                      // add the client to the container
+        }
+    }
+
+    public class Application
+    {
+        IPwnedPasswordsClient _client;
+
+        public Application(IPwnedPasswordsClient client)
+        {
+            _client = client;
+        }
+
+        public async Task Verify(string password)
+        {
+            var result = await _client.HasPasswordBeenPwned(password);
+            Console.WriteLine(result);
+        }
+    }
+}
